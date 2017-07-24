961d332e271b62d69bc532079b2f476fdfecaae8
Charlie Kelson, Jesse Bryan and Michael Woldemedihin <jesse@>
2017-07-24 13:29:15 -0700

    add functionality to business and user interface logic.

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ var teamOne = [];
 var teamTwo = [];
 var unpickedPlayers = [];
 var names = ["Sam", "John", "Amy", "Jaime", "Sarah", "Tom", "Jesse", "Bob", "David", "Tari", "William", "Harrison", "Victor", "Thomas", "Larry", "Robin"];
+var lastNames = ["Smith", "Johnson", "Jones", "Brown", "Davis", "Moore", "Williams", "Taylor", "Thompson", "Garcia", "Nelson", "Carter", "Rodriguez", "Lewis", "Lee", "Walker"];
 var sex = ["Male", "Female"];
 
 function Players(names, age, sex, height, battingAvg) {
@@ -10,19 +11,20 @@ function Players(names, age, sex, height, battingAvg) {
   this.sex = sex;
   this.playerHeight = height;
   this.battingAvg = battingAvg;
-
-function Team(){
-  var players = [];
 }
+
+// function Team(){
+//   var players = [];
+// }
 
 var createPlayerList = function(amount){
   for (i = 0; i < amount; i++){
-    var playerName = names[Math.floor(Math.random() * names.length)];
+    var playerName = names[Math.floor(Math.random() * names.length)] + " " + lastNames[Math.floor(Math.random() * lastNames.length)];
     var age = Math.floor(Math.random() * 27) + 18;
-    var sex = Math.floor(Math.random() * 2);
+    var sexPlayer = sex[Math.floor(Math.random() * 2)];
     var height = Math.floor(Math.random() * 18) + 60;
     var battingAvg = Math.floor(Math.random() * 320) + 50;
-    var newPlayer = new Player(playerName, age, sex, height, battingAvg);
+    var newPlayer = new Players(playerName, age, sexPlayer, height, battingAvg);
     unpickedPlayers.push(newPlayer);
   }
 }
@@ -31,6 +33,10 @@ var createPlayerList = function(amount){
 
 // User logic
 $(function(){
+  createPlayerList(30);
+  for (i = 0; i < unpickedPlayers.length; i++) {
+    $(".sidebar ul").append("<li data-player=" + i + ">" + unpickedPlayers[i].playerName + "</li>")
+  }
 // var player1 = new Player();
 // var player2 = new Player();
 // if (team1 clicked) {
@@ -40,10 +46,9 @@ $(function(){
 // }
 
   $(".player li").click(function(){
-    createPlayerList();
-
     var currentPlayer = $(this).html();
-    console.log(currentPlayer);
-    $("#details ul").append('<li>' + currentPlayer + '</li');
+    var unpickedPlayersIndex = ($(this).data("player"));
+    $(".details h2").text(unpickedPlayers[unpickedPlayersIndex].playerName);
+
   });
 });
